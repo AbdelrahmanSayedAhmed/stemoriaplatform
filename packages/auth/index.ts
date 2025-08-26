740cf88fd988abebbfb2384bcbeac411a32f52c0
AbdelrahmanSayedAhmed <abdelrahmanmohamed@science.menofia.edu.eg>
2025-08-26 03:27:50 +0300

    Fix export statements in auth index file

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,4 +1,4 @@
 export { default } from 'next-auth'
 export { getServerSession } from 'next-auth'
 export * from './lib/auth-config'
-export * from './lib/providers'S
+export * from './lib/providers'
