08fb7065ac4972a2369fe85ca46ccce92bd78a28
AbdelrahmanSayedAhmed <abdelrahmanmohamed@science.menofia.edu.eg>
2025-08-26 03:34:10 +0300

    Create feature.tsx

---
+++ packages/ui/src/components/stemoria/feature.tsx
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../card"
+import { cn } from "../../lib/utils"
+
+interface FeatureCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title: string
+  description: string
+  icon?: React.ReactNode
+}
+
+const FeatureCard = React.forwardRef<HTMLDivElement, FeatureCardProps>(
+  ({ className, title, description, icon, ...props }, ref) => {
+    return (
+      <Card ref={ref} className={cn("h-full", className)} {...props}>
+        <CardHeader>
+          {icon && (
+            <div className="mb-2 flex h-12 w-12 items-center justify-center rounded-lg bg-primary/10">
+              {icon}
+            </div>
+          )}
+          <CardTitle className="text-xl">{title}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="text-base leading-relaxed">
+            {description}
+          </CardDescription>
+        </CardContent>
+      </Card>
+    )
+  }
+)
+FeatureCard.displayName = "FeatureCard"
+
+export { FeatureCard }
